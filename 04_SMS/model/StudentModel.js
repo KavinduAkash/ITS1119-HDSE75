095794b33add67d36c8661316c881655324fedb4
kavinduakash <bkkasamarasingha1220@gmail.com>
2026-04-23 16:32:03 +0530

    update sms

--- 04_SMS/model/StudentModel.js
+++ 04_SMS/model/StudentModel.js
@@ -1,18 +1,68 @@
 import {student_db} from '../db/db.js';
 
+class Student {
+    #id;
+    #name;
+    #nic;
+    #phone;
+    #address;
+
+    constructor(id, name, nic, phone, address) {
+        this.#id = id;
+        this.#name = name;
+        this.#nic = nic;
+        this.#phone = phone;
+        this.#address = address;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get nic() {
+        return this.#nic;
+    }
+
+    get phone() {
+        return this.#phone;
+    }
+
+    get address() {
+        return this.#address;
+    }
+
+    set id(id) {
+        this.#id = id;
+    }
+
+    set name(name) {
+        this.#name = name;
+    }
+
+    set nic(nic) {
+        this.#nic = nic;
+    }
+
+    set phone(phone) {
+        this.#phone = phone;
+    }
+
+    set address(address) {
+        this.#address = address;
+    }
+}
+
 // --------------------------- Add Student ---------------------------
  const addStudentData = (sid, sname, snic, sphone, saddress) => {
-     let new_student = {
-         id: sid,
-         name: sname,
-         nic: snic,
-         phone: sphone,
-         address: saddress
-     };
+     let new_student = new Student(sid, sname, snic, sphone, saddress);
      student_db.push(new_student);
  }
 
- // --------------------------- Update Student ---------------------------
+// --------------------------- Update Student ---------------------------
 const updateStudentData = (sid, sname, snic, sphone, saddress) => {
     let obj = student_db.find(item => item.id == sid);
 
@@ -24,7 +74,7 @@ const updateStudentData = (sid, sname, snic, sphone, saddress) => {
     }
 }
 
- // --------------------------- Delete Student ---------------------------
+// --------------------------- Delete Student ---------------------------
 const deleteStudentData = (sid) => {
     let index = student_db.findIndex(item => item.id == sid); // -1
 
@@ -33,7 +83,7 @@ const deleteStudentData = (sid) => {
     }
 }
 
- // --------------------------- Get Student ---------------------------
+// --------------------------- Get Student ---------------------------
  const getStudentData = () => {
     return student_db;
  }
@@ -48,4 +98,4 @@ const getStudentDataById = (id) => {
     return student_db.find(item => item.id==id);
 }
 
- export {addStudentData, updateStudentData, deleteStudentData, getStudentData, getStudentDataByIndex, getStudentDataById};
+export {addStudentData, updateStudentData, deleteStudentData, getStudentData, getStudentDataByIndex, getStudentDataById};
