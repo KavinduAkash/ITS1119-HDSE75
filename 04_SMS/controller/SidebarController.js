145eb69ecae12917c74a93fffcb8faebcdaa4955
kavinduakash <bkkasamarasingha1220@gmail.com>
2026-04-23 18:26:50 +0530

    fix content margin issue

--- 04_SMS/controller/SidebarController.js
+++ 04_SMS/controller/SidebarController.js
@@ -3,11 +3,11 @@ let sidebar_open = true;
 $('#menu_btn').on('click', function () {
     if(sidebar_open) {
         $('#sidebar').css({display: 'none'});
-        $('#content').css({marginLeft: '0px'});
+        $('.content').css({marginLeft: '0px'});
         sidebar_open = false;
     } else {
         $('#sidebar').css({display: 'block'});
-        $('#content').css({marginLeft: '250px'});
+        $('.content').css({marginLeft: '250px'});
         sidebar_open = true;
     }
 })
