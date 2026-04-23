906889a09dc1a94f4ae3d7fbfdaad9d2eeab5ecf
kavinduakash <bkkasamarasingha1220@gmail.com>
2026-04-23 16:56:33 +0530

    update sms

--- 04_SMS/controller/SidebarController.js
+++ 04_SMS/controller/SidebarController.js
@@ -11,4 +11,16 @@ $('#menu_btn').on('click', function () {
         sidebar_open = true;
     }
 })
+
+$('#course_content').css('display', 'none');
+
+$('#student_sidebar_tab').on('click', function () {
+    $('#course_content').css('display', 'none');
+    $('#student_content').css('display', 'block');
+})
+
+$('#course_sidebar_tab').on('click', function () {
+    $('#course_content').css('display', 'block');
+    $('#student_content').css('display', 'none');
+})
 // ------------------------ Sidebar Handler --------------------------
